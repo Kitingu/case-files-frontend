854958221ead9f2a06fad72214d5896f056609f3
hezronkimutai <hezronchelimo.hc@gmail.com>
2020-06-07 20:37:38 +0200

    about patners

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,13 +1,14 @@
-import React from 'react'
-import AboutHero from '../../about-us/About-hero'
-import AboutDetails from '../../about-us/About-details'
-
- const About = () => {
-    return (
-        <div>
-           <AboutHero/>
-           <AboutDetails/>
-        </div>
-    )
-}
+import React from "react";
+import AboutHero from "../../about-us/About-hero";
+import AboutDetails from "../../about-us/About-details";
+import AboutPatners from "../../about-us/AboutPatners";
+const About = () => {
+  return (
+    <div>
+      <AboutHero />
+      <AboutDetails />
+      <AboutPatners />
+    </div>
+  );
+};
 export default About;
