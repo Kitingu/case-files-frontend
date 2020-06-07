6707bb0476e2cd392707b0692e9a6b8ef720ba1f
kitingu <benlegendj@gmail.com>
2020-06-07 07:53:14 +0300

    ft(add about-us hero image) [starts #173210534]

---
+++ src/about-us/About-details.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import '../assets/About-us-page.scss'
+
+const AboutDetails = () => {
+  return (
+    <div className="jumbotron">
+      <div className="about-details">
+        <h2> About Justice Networks </h2>
+        <p>
+          Justice Networks features research and documentation on justice, human
+          rights, democracy and peace in Eastern Africa. We highlight the
+          initiatives of researchers and activists to document and promote civic
+          and legal activism in the region in contexts of violent conflict,
+          human rights abuse and humanitarian crisis. We host an archive of
+          primary data gathered during academic research projects on courts and
+          legal activism in South Sudan and Somalia in the period between 2015
+          and 2020. Our aims are to:
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutDetails;
