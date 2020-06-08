f6ec0e3e74b65fe8003767a5e95fc0179cc097bc
kitingu <benlegendj@gmail.com>
2020-06-08 20:53:15 +0300

    update slider content

--- src/about-us/AboutSlider.js
+++ src/about-us/AboutSlider.js
@@ -1,43 +1,36 @@
 import React, { useEffect, useState } from "react";
+import ReadMoreReact from 'read-more-react';
 import "../assets/aboutSlider.scss";
 
 import back from "../images/back.png";
 import next from "../images/next.png";
 const data = [
   {
-    header: "Provide1 insights from research into the law inpractice",
+    header: 'Provide insights from research into the law inpractice',
     content:
-      "in conflict and crisis settings in Eastern Africa, where most people only have access to localcourts and where custmary or religious laws and authoritiesoften command legitimacy",
+      'in conflict and crisis settings in Eastern Africa, where most people only have access to local courts; and where customary or religious laws and authorities often command legitimacy. Our research demonstrates that efforts to deliver and promote ‘law from below’ continue despite conflict and ‘fragility.’ Documenting court proceedings can enrich our understanding of law, politics and society; it can also enable legal empowerment [link to https://namati.org/]  public scrutiny, and social accountability across the region.',
   },
   {
-    header: "Provide2 insights from research into the law inpractice",
+    header: 'Share knowledge and research materials',
     content:
-      "in conflict and crisis settings in Eastern Africa, where most people only have access to localcourts and where custmary or religious laws and authoritiesoften command legitimacy",
+      'to expand and enrich understanding of the work of local courts and the struggles of civic and legal activists in conflict settings in Eastern Africa.',
   },
   {
-    header: "Provide3 insights from research into the law inpractice",
+    header:
+      'Serve as a repository of primary data from internationally-funded research projects',
     content:
-      "in conflict and crisis settings in Eastern Africa, where most people only have access to localcourts and where custmary or religious laws and authoritiesoften command legitimacy",
+      'to enable stakeholders in the region to access, use and interpret these materials. Too often materials gathered through costly and difficult research with participants in Eastern Africa is analysed from a single (usually external) perspective. We believe that novel and ethical ways must be found to widen access to primary data.',
   },
   {
-    header: "Provide4 insights from research into the law inpractice",
+    header: 'Foster networking and strengthen civic and legal activism',
     content:
-      "in conflict and crisis settings in Eastern Africa, where most people only have access to localcourts and where custmary or religious laws and authoritiesoften command legitimacy",
+      'among community paralegals, lawyers, and civil society and community activists committed to justice in Eastern Africa. Their work may be voluntary, and is sometimes dangerous, yet they make practical and timely contributions to the protection and human rights of citizens.',
   },
   {
-    header: "Provide5 insights from research into the law inpractice",
+    header:
+      'Connect activists to researchers and encourage participatory action research',
     content:
-      "in conflict and crisis settings in Eastern Africa, where most people only have access to localcourts and where custmary or religious laws and authoritiesoften command legitimacy",
-  },
-  {
-    header: "Provide6 insights from research into the law inpractice",
-    content:
-      "in conflict and crisis settings in Eastern Africa, where most people only have access to localcourts and where custmary or religious laws and authoritiesoften command legitimacy",
-  },
-  {
-    header: "Provide7 insights from research into the law inpractice",
-    content:
-      "in conflict and crisis settings in Eastern Africa, where most people only have access to localcourts and where custmary or religious laws and authoritiesoften command legitimacy",
+      '<a href="https://www.participatorymethods.org/task/research-and-analyse"/> as a viable and ethical method of contributing to academic knowledge.',
   },
 ];
 export default () => {
@@ -70,13 +63,13 @@ export default () => {
         className="controlButton"
       >
         <img src={back} alt="back" />
-      </button>{" "}
+      </button>{' '}
       <div className="slides">
         {slideData.map((slide) => (
           <div className="slide">
             <p>
               <strong>{slide.header} </strong>
-              {slide.content}
+              <ReadMoreReact text= {slide.content} />
             </p>
           </div>
         ))}
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -11,6 +11,7 @@ const About = () => {
       <AboutDetails />
       <AboutSlider />
       <AboutPatners />
+      
     </div>
   );
 };
